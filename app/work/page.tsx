100a4286e0b5217adff400df278a41b2e923d918
mahautlatinis <mahautlatinisheritier@gmail.com>
2024-12-10 19:02:28 +0100

    wip

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -15,7 +15,7 @@ export default function WorkPage() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione facere vel tempore. Nam nobis earum quasi ipsam dignissimos, laboriosam, sint dolores quis, ut quod expedita ea quibusdam voluptates necessitatibus corporis.
         </p>
         <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <h2 className="font-medium text-xl mb-1 tracking-tighter">Vercel</h2>
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Greenly</h2>
         <p className="text-neutral-600 dark:text-neutral-400 text-sm">
           Software Engineer
         </p>
@@ -37,25 +37,7 @@ export default function WorkPage() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae veritatis corporis architecto asperiores odio alias nesciunt nemo, vitae quas optio nulla odit accusamus porro, in ad rem beatae magnam? Quo
         </p>
         <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <h2 className="font-medium text-xl mb-1 tracking-tighter">Hy-Vee</h2>
-        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga ullam, animi minima voluptates deleniti eaque praesentium quaerat aspernatur voluptate repellat tenetur maiores mollitia consequuntur debitis obcaecati fugit quae molestiae eius.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo id fuga repudiandae enim officia impedit non nulla cupiditate consequuntur incidunt. Possimus facere optio nam temporibus eaque aliquid nobis maiores deleniti.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, animi! Illo, nostrum nulla ipsum distinctio qui reiciendis voluptatibus quasi magnam id accusamus explicabo sint facilis! Illum inventore beatae sed corrupti.
-        </p>
-        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <h2 className="font-medium text-xl mb-1 tracking-tighter">Workiva</h2>
-        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-          Lorem
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate in enim non! Voluptate minus neque officiis commodi. Facere nisi assumenda similique unde perspiciatis. Repellat minima aspernatur minus ipsum expedita officia?
-        </p>
-      </div>
-    </section>
-  );
-}
+        </div>
+      </section>
+      )
+    }
