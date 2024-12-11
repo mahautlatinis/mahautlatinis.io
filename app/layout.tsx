28cf8cacdb8a81d0812ebc245181819da3755bed
mahautlatinis <mahautlatinisheritier@gmail.com>
2024-12-11 11:33:36 +0100

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { Navbar } from './components/nav';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 /* 
 TODO: a revoir 
@@ -67,8 +65,6 @@ export default function RootLayout({
             <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
               <Navbar />
               {children}
-              <Analytics />
-              <SpeedInsights />
             </main> 
           </NextUIWrapper>
         </body>
